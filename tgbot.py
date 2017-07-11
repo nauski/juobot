64bc4ed931275d0869c1257807304a2f7d27c53a
nauski <nauski@nauski.com>
2017-07-11 19:32:59 +0300

    added long polling and parsing string to url

--- tgbot.py
+++ tgbot.py
@@ -1,7 +1,8 @@
 import json
 import requests
 import os
-
+import time
+from urllib.parse import urlparse
 
 TOKEN = os.environ["TGBOT_TOKEN"] 
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -16,8 +17,10 @@ def get_json_from_url(url):
     js = json.loads(content)
     return js
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
 
@@ -29,8 +32,33 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 def send_message(text, chat_id):
+    text = urlparse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+
+
+if __name__ == '__main__':
+    main()
